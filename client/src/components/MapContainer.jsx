8cd49fb494cd84755be63fe2074b8bf8a9594aa0
abel30567 <claudio@imarkett.com>
2018-11-04 05:23:31 -0500

    render streetview

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -7,7 +7,8 @@
 import React from "react";
 import Popup from 'reactjs-popup';
 import ReactDOM from 'react-dom';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper, StreetViewPanorama } from "google-maps-react";
+
 
 class MapContainer extends React.Component {
   constructor(props) {
@@ -19,6 +20,11 @@ class MapContainer extends React.Component {
         lat: 25.7954910, 
         lng: -80.386,
       },
+      pov: {
+        heading: 280,
+        pitch: 0,
+      },
+      showingStreetView: false,
       showingInfoWindow: false,
       activeMarker: {},
       selectedPlace: {},
@@ -27,6 +33,7 @@ class MapContainer extends React.Component {
       markers: props.markers,
     };
     this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMarkerClickToo = this.onMarkerClickToo.bind(this);
     this.getCenter = this.getCenter.bind(this);
     this.noVideo = this.noVideo.bind(this);
     this.streetView = this.streetView.bind(this);
@@ -65,15 +72,15 @@ class MapContainer extends React.Component {
     });
   }
 
-  // onMarkerClickToo(props, map, event) {
-  //   // console.log("Latitude: ", event.latLng.lat());
-  //   // console.log("Longitude: ", event.latLng.lng());
-  //   // console.log(event, 'event');
-  //   // console.log(props, 'marker props')
-  //   // props.video gets you src
-  //   this.props.click(props.video);
-  //   // console.log(this.props);
-  // }
+  onMarkerClickToo(props, map, event) {
+    // console.log("Latitude: ", event.latLng.lat());
+    // console.log("Longitude: ", event.latLng.lng());
+    // console.log(event, 'event');
+    // console.log(props, 'marker props')
+    // props.video gets you src
+    this.props.click(props.video);
+    // console.log(this.props);
+  }
 
   noVideo() {
       this.props.noVideo();
@@ -89,7 +96,36 @@ class MapContainer extends React.Component {
     console.log(node);
     node.setAttribute('style', 'display:none');
   }
+  
+  onInfoWindowOpen(props, e) {
+    const button = (<button onClick={(e) => {
 
+      console.log(this, 'this')
+      console.log(props, 'props of map')
+      const node = ReactDOM.findDOMNode(this);
+      const panorama = new props.google.maps.StreetViewPanorama(document.getElementById('pano'), {
+        position: this.state.position,
+        pov: this.state.pov,
+        linksControl: false,
+        visible: true,
+      panControl: false,
+      enableCloseButton: false,
+      zoomControl: false,
+      fullscreenControl: false
+      });
+      console.log(panorama);
+      panorama.setVisible(true);
+      document.getElementById('secondFrame').setAttribute('style', 'display: block; position: absolute; top: 50vh; left: 50vw; z-index: 2000')
+      document.getElementById('backToMap').setAttribute('style', 'display: block; position: absolute; top: 100px; left: 25px; z-index: 2010; color: red; background-color: white; border: 2px solid red; border-radius: 5px;')
+    }}>mapbutton</button>);
+    ReactDOM.render(React.Children.only(button), document.getElementById("iwc"));
+  }
+  backToMap() {
+    document.getElementById('secondFrame').setAttribute('style', 'display: none;');
+    document.getElementById('backToMap').setAttribute('style', 'display: none;');
+    document.getElementById('pano').setAttribute('style', 'display: none;');
+
+  }
   render() {
       
       const mapStuff = this.state.markers.map(marker => 
@@ -97,33 +133,45 @@ class MapContainer extends React.Component {
                 key={marker.position.lat}
                 video={marker.url}
                 position={marker.position}
-                onClick={this.onMarkerClick}
+                onClick={this.onMarkerClickToo}
               />)
           );
-      // const money = <Marker position={this.props.position} onClick={this.showMagic}/>;
-      // const money = (<div><</div>);
-    //   const money = (<Popup
-    //   trigger= {<Marker position={this.props.position} />}
-    //   open={true}
-    //   position="top center"
-    //   on="hover"
-    // >
-    //   <iframe width="560" height="315" src="https://www.youtube.com/embed/tK4W-V34DFo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    // </Popup>);
-
-
-    
+          const money = (<div><Marker 
+            map={this.props.map}
+            position={this.state.position} 
+            onClick={this.onMarkerClick}
+            name={'Current location'} 
+            />
+  
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onOpen={e => {
+              this.onInfoWindowOpen(this.props, e);
+            }}
+            >
+              <div>
+                <iframe style={{ zIndex: '1610' }} width="300px" height="100%" src="https://www.youtube.com/embed/tK4W-V34DFo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div style={{ width: '300px'}}>
+                  <h4 style={{ fontSize: '1rem' }}>Abre sus puertas la nueva sede de Telemundo en Miami | Noticiero | Telemundo</h4>
+                  <p>Video oficial de Noticias Telemundo. Con un costo de más de 250 millones de dólares, el nuevo centro global de operaciones Telemundo Center albergará a 1500 empleados en sus 13 estudios.</p>
+                  <div id="iwc" />
+                </div>
+              </div>
+          </InfoWindow></div>)
 
     return (
-      <div style={{ width: '80vw', height: '80vh' }} className="col-xs-12">
+      
+      <div style={{ width: '100vw', height: '100vh' }} className="col-xs-12">
         {/* <div className="col-xs-12">
         <iframe style={{ position: 'absolute', top: '50vh', left: '50vw', zIndex: '1500' }} width="10%" height="10%" src="https://www.youtube.com/embed/tK4W-V34DFo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div> */}
         
+        
         <Map
         google={this.props.google}
         zoom={16}
-        onDragend={this.getCenter}
+        onDragend={this.getCenter} 
         onClick={this.noVideo}
         initialCenter={{
           lat: this.props.position.lat,
@@ -132,27 +180,12 @@ class MapContainer extends React.Component {
         style={{ width: "100%", height: "100%", zIndex: '100' }}
       >
         {mapStuff}
-        <Marker 
-          position={this.state.position} 
-          onClick={this.onMarkerClick}
-          name={'Current location'} 
-          />
-
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-            <div>
-              <iframe style={{ zIndex: '1500' }} width="300px" height="100%" src="https://www.youtube.com/embed/tK4W-V34DFo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              <div style={{ width: '300px'}}>
-                <h4 style={{ fontSize: '1rem' }}>Abre sus puertas la nueva sede de Telemundo en Miami | Noticiero | Telemundo</h4>
-                <p>Video oficial de Noticias Telemundo. Con un costo de más de 250 millones de dólares, el nuevo centro global de operaciones Telemundo Center albergará a 1500 empleados en sus 13 estudios.</p>
-                <a onClick={this.streetView} href="#" className="btn btn-primary">street</a>
-              </div>
-            </div>
-        </InfoWindow>
+        {money}
       </Map>
+      <a onClick={this.backToMap} id="backToMap" className="btn" style={{ zIndex: '1610', display: 'none', borderRadius: '5px', border: '1px solid red', backgroundColor: 'white', color: 'red' }}>Regresar</a>
+      <iframe id="secondFrame" style={{ zIndex: '1610', display: 'none' }} width="300px" height="200px" src="https://www.youtube.com/embed/tK4W-V34DFo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div id="pano" style={{width: '100%', height: '100%', zIndex: '100'}}></div>
       </div>
-      
     );
   }
 }
